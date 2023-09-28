8cdc4dfcf13c5ca72b63cc5d5c5e54edd2aeda86
Your Name~kunal-511 <yoyokvunal@gmail.com>
2023-09-28 11:48:19 +0530

    last

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -71,15 +71,15 @@ const Header: React.FC = () => {
             <li className="text-2xl">Evmos</li>
           </ul>
           <ul className="flex space-x-8 justify-center items-center font-semibold">
-          {!isSticky && (
+      
             <li
               onMouseEnter={handleMouseEnter}
               onMouseLeave={handleMouseLeave}
-              className="flex justify-center items-center remove"
+              className={`flex justify-center items-center ${isSticky ? 'hidden' : ''}`}
             >
               Technology <BiDownArrowAlt className="pt-[4px] text-2xl" />
             </li>
-          )}
+         
             <li
               onMouseLeave={handleMouseLeave}
               className=" flex justify-center items-center"
